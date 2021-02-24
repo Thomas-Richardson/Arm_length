596f522ce54b5125f2dfa572d45ee74964eba45f
Thomas Richardson <thomasrichardson140293@outlook.com>
2021-02-24 16:29:24 +0000

    Delete install.R

--- install.R
+++
@@ -1,7 +0,0 @@
-install.packages('ggplot2')
-install.packages('readr')
-install.packages('dplyr')
-install.packages('magrittr')
-install.packages('visreg')
-install.packages('broom')
-install.packages('purrr')
