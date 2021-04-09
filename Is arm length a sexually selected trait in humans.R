29ae81269e7db5fbe05937e774fd1c86f147ed32
Thomas Richardson <thomasrichardson140293@outlook.com>
2021-04-09 13:19:35 +0100

    Added more comments to code
    Added more comments to code

--- Is arm length a sexually selected trait in humans.R
+++ Is arm length a sexually selected trait in humans.R
@@ -18,7 +18,7 @@ library(tidyr)
 #library(gender)
 #=read_csv('MMA_data.csv') # read in unsexed data
 
-#data_MMA$sex_weight = 'unknown'
+#data_MMA$sex_weight = 'unknown' # create a column of fighter's sex based on their weight. We know there are no female fighters >145lbs and no males <116. We can use this to 'sanity check' the sex later
 #data_MMA$sex_weight[data_MMA$Weight<116] = 'female' # classify anyone at 115lb as female as there are no male weight classes this low
 #data_MMA$sex_weight[data_MMA$Weight>145] = 'male' # classify anyone above 145lb as male as there are no female weight classes this high
 
@@ -34,7 +34,7 @@ library(tidyr)
 
 #write_csv(data_MMA,'MMA_data_sexed_.csv')
 
-# note that it will fail to identify around 200 fighters which whose sex I then googled.
+# note that it will fail to identify around 200 fighters which whose sex I then googled... Better than doing over 1000!
 
 #============ Lets stats! =======================================================================
 cbPalette_ <- c("#E69F00", "#56B4E9", "#999999","#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7") # define a colour palette for graph that is friendly to people with red-green colour blindness
@@ -53,7 +53,7 @@ men = data %>% filter(sex=='male')   # filter men only
 
 data$sex %>% table # how many men vs women
 
-
+# plot density plots for each numeric variable to look for anomolies.
 data %>%                                        # plot all numeric variables as density plots. 
   keep(is.numeric) %>%  # keep numeric columns
   gather() %>%       # stack them on top of each other (just a hack to make the next line work)
@@ -63,8 +63,8 @@ data %>%                                        # plot all numeric variables as 
 # Feel free to use it on your own data and share, I didn't write it :)
 
 
-cor.test(women$Armspan,women$Height) # correlation of armspand and height in women
-cor.test(men$Armspan,men$Height)       # correlation of armspand and height in men
+cor.test(women$Armspan,women$Height) # correlation of armspan and height in women
+cor.test(men$Armspan,men$Height)       # correlation of armspan and height in men
 cor.test(data$AHR,data$Weight,method = 'spearman')      # correlation between armspan to height ratio and weight, to test for positive allometry 
 cor.test(data$AHR,data$Height)
 
@@ -84,6 +84,7 @@ ggsave('sexual_dimorphism_armspan.jpg',plot=dimorphism_plot,dpi = 600,width = 15
 
 mod = lm(Win_Percentage~Armspan*sex+Height+Weight,data) # multiple regression predicting win percentage from height weight armspan and sex, also armspan*sex interaction
 mod %>% summary # interaction isn't sig, so we remove it
+
 mod = lm(Win_Percentage~Armspan+sex+Height+Weight,data) 
 results = tidy(mod,conf.int=T) %>% select(-statistic) %>%  rename(Predictor = term, b = estimate, se = std.error, `p-value`=p.value) # tidy up the regression output 
 results[2:5,] # print out tidy regression output
